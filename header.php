7c00f60b9e2128d25b132c7015c9c66a8937c7a2
Cornell van der Straaten <30183@ma-web.nl>
2020-09-14 12:52:13 +0200

    Ready to go

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php echo language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name') ?></title>
+    <?php wp_head() ?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+</head>
+
+<body <?php body_class() ?>>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header() ?>
+
+<div id="fullpage">
+  <div class="section" id="section1">
+    <div class="section-content">
+      <h1><i class="fa fa-check-circle-o"></i></h1>
+      <h1>Welcome to My SITE</h1>
+    </div>
+  </div>
+  <div class="section" id="section2">section2</div>
+  <div class="section" id="section4">section4</div>
+</div>
+
+<?php get_footer() ?>
