9ed2f3a0cb85e61c46eb471b86c48b8dc8980afe
MeesPos <meespostma_@hotmail.com>
2020-09-14 16:59:52 +0200

    Header is kut

--- index.php
+++ index.php
@@ -3,26 +3,30 @@
 <div id="fullpage">
   <div class="section" id="header">
     <header>
-      <div class="row d-flex">
-        <div class="headerContent col-xl-6">
-          <h1>Power Marketing.</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus
-            metus vel congue viverra. Donec sit amet ante a dolor vestibulum luctus.</p>
-          <a href="#" class="bedrijfsVideo">
-            <i class="fas fa-play-circle"></i>
-            <p>Bekijk onze bedrijfsvideo!</p>
-          </a>
+      <div class="vertical">
+        <div class="headerGrid">
+          <div class="verticale">
+            <div class="headerContent">
+              <h1>Power Marketing.</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus
+                metus vel congue viverra. Donec sit amet ante a dolor vestibulum luctus.</p>
+              <a href="#" class="bedrijfsVideo">
+                <i class="fas fa-play-circle"></i>
+                <p>Bekijk onze bedrijfsvideo!</p>
+              </a>
+            </div>
+
+            <div class="headerIllu">
+              <img src="<?php echo get_template_directory_uri() . '/img/headerIllu.png' ?>" alt="Illustration van de Header - Power Marketing">
+            </div>
+          </div>
         </div>
-
-        <div class="col-xl-6">
-
-        </div>
-
       </div>
     </header>
   </div>
-  <div class="section" id="section2">section2</div>
-  <div class="section" id="section3">section4</div>
+</div>
+<div class="section" id="section2">section2</div>
+<div class="section" id="section3">section4</div>
 </div>
 
 <?php get_footer() ?>
