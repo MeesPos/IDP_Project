7feffca7073b5b62d19670281d54e6a52a01e3f6
Cornell van der Straaten <30183@ma-web.nl>
2020-09-14 21:38:44 +0200

    Contact section done ( not responsive )

--- index.php
+++ index.php
@@ -8,7 +8,26 @@
     </div>
   </div>
   <div class="section" id="section2">section2</div>
-  <div class="section" id="section4">section4</div>
+  <div class="section contact-section" id="section3">
+    <div class="contact-page-left">
+      <div></div>
+
+      <div class="contact-logo">
+        <img src="<?php echo get_template_directory_uri() . '/img/logo.svg' ?>" alt="Big purple letter P with a lightning bolt in it.">
+      </div>
+
+      <div></div>
+    </div>
+    <div class="contact-page-right">
+      <div></div>
+      <div class="contact-page-right-container">
+        <h2>NEEM CONTACT OP</h2>
+        <?php echo do_shortcode('[wpforms id="8" title="false" description="false"]') ?>
+      </div>
+      <div></div>
+    </div>
+  </div>
+
 </div>
 
 <?php get_footer() ?>
