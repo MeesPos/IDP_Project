d3bec92d1df36611ffbc01f077b03170c1fe57fb
MeesPos <meespostma_@hotmail.com>
2020-09-15 18:19:26 +0200

    Header bijna af, beginnen met nav

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <?php get_header() ?>
 
 <div id="fullpage">
-  <div class="section" id="header">
-    <header>
-      <div class="vertical">
-        <div class="headerGrid">
-          <div class="verticale">
-            <div class="headerContent">
-              <h1>Power Marketing.</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus
-                metus vel congue viverra. Donec sit amet ante a dolor vestibulum luctus.</p>
-              <a href="#" class="bedrijfsVideo">
-                <i class="fas fa-play-circle"></i>
-                <p>Bekijk onze bedrijfsvideo!</p>
-              </a>
-            </div>
+  <div class="section" id="section1">
+    <section id="homeHeader">
+      <div class="container-flex">
+        <div class="row">
+          <div class="headerContent col-xl-6">
+            <h1>Power Marketing.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras maximus
+              metus vel congue viverra. Donec sit amet ante a dolor vestibulum luctus.</p>
+            <a href="#" class="bedrijfsVideo">
+              <i class="fas fa-play-circle"></i>
+              <p>Bekijk onze bedrijfsvideo!</p>
+            </a>
+          </div>
 
-            <div class="headerIllu">
-              <img src="<?php echo get_template_directory_uri() . '/img/headerIllu.png' ?>" alt="Illustration van de Header - Power Marketing">
-            </div>
+          <div class="headerIllu col-xl-6 text-center">
+            <img src="<?php echo get_template_directory_uri() . '/img/headerIllu.png' ?>" alt="Illustration van de Header - Power Marketing">
           </div>
         </div>
       </div>
-    </header>
+    </section>
   </div>
 </div>
 <div class="section" id="section2">section2</div>
