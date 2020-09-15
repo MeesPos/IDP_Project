f79e4d4ca7f6ffcb3ec7065bac9c00ed27f84bff
Cornell van der Straaten <30183@ma-web.nl>
2020-09-15 20:55:18 +0200

    Team page 90% working
    Still need to get To Portfolio right and work on responsiveness

--- index.php
+++ index.php
@@ -11,13 +11,71 @@
 
   <div class="section team-section" id="section3">
     <div class="team-section-container">
-      <h2>ONS TEAM</h2>
-      <p>Yo hier staat ons team</p>
+      <h2>Maak kennis met ons team</h2>
+      <p>Het team van Power marketing bestaat uit 4 enthusiaste medewerkers.
+        Zij staan iedere werkdag voor u klaar, of u nu al klant bent of
+        graag meer wilt weten over wat wij doen!
+      </p>
+      
       <div class="team-container">
-        <div class="team-kaartje">
-          <img src="" alt="">
-          <h3></h3>
+        <div class="team-grid-item">
+          <div class="team-kaartje">
+            <img src="<?php echo get_template_directory_uri() . '/img/cornell.jpg' ?>" alt="Een foto van designer Larissa van Rijn.">
+            <h3 class="team-naam">Cornell van der Straaten</h3>
+            <p class="team-functie">Developer</p>
+            <a href="#" class="team-link">
+              <div class="team-portfolio-link-arrow-div">
+                <img src="<?php echo get_template_directory_uri() . '/img/arrow.png' ?>" alt="">
+                <p>Naar Portfolio</p>
+              </div>
+            </a>
+          </div>
         </div>
+
+        <div class="team-grid-item">
+          <div class="team-kaartje">
+            <img src="<?php echo get_template_directory_uri() . '/img/mees.jpg' ?>" alt="Een foto van designer Larissa van Rijn.">
+            <h3 class="team-naam">Mees Postma</h3>
+            <p class="team-functie">Developer</p>
+            <a href="#" class="team-link">
+              <div class="team-portfolio-link-arrow-div">
+                <img src="<?php echo get_template_directory_uri() . '/img/arrow.png' ?>" alt="">
+                <p>Naar Portfolio</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="team-grid-item">
+          <div class="team-kaartje">
+            <img src="<?php echo get_template_directory_uri() . '/img/larissa.jpeg' ?>" alt="Een foto van designer Larissa van Rijn.">
+            <h3 class="team-naam">Larissa van Rijn</h3>
+            <p class="team-functie">Graphic Designer</p>
+            <a href="#" class="team-link">
+              <div class="team-portfolio-link-arrow-div">
+                <img src="<?php echo get_template_directory_uri() . '/img/arrow.png' ?>" alt="">
+                <p>Naar Portfolio</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div class="team-grid-item">
+          <div class="team-kaartje">
+            <img src="<?php echo get_template_directory_uri() . '/img/larissa.jpeg' ?>" alt="Een foto van designer Larissa van Rijn.">
+            <h3 class="team-naam">Mitchel Westerwaal</h3>
+            <p class="team-functie">Graphic Designer</p>
+            <a href="#" class="team-link">
+              <div class="team-portfolio-link-arrow-div">
+                <img src="<?php echo get_template_directory_uri() . '/img/arrow.png' ?>" alt="">
+                <p>Naar Portfolio</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+
+
       </div>
     </div>
   </div>
