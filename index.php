70f3f12cfea4c3470ec1bc7188029abd0edd62a5
Cornell van der Straaten <30183@ma-web.nl>
2020-09-21 14:15:13 +0200

    footer done and responsive

--- index.php
+++ index.php
@@ -202,19 +202,28 @@
             congue viverra.</p>
         </div>
         <div class="footer-contact "><?php dynamic_sidebar('footer-contact'); ?></div>
-        <div class="footer-verkennen "><?php
-                                        wp_nav_menu(array(
-                                          'theme_location'    => 'footer-menu',
-                                          'depth'             => 2,
-                                          'container'         => 'div',
-                                          'menu_class'        => 'footerMenu',
-                                        ));
-                                        ?></div>
+        <div class="footer-verkennen ">
+          <h3>Verkennen</h3>
+          <?php
+          wp_nav_menu(array(
+            'theme_location'    => 'footer-menu',
+            'depth'             => 2,
+            'container'         => 'div',
+            'menu_class'        => 'footerMenu',
+          ));
+          ?>
+        </div>
       </div>
       <hr>
-      <div class="row">
+      <div class="row footer-row">
         <div class="footer-copyright col">2020 © by Power Marketing</div>
-        <div class="footer-socials col">Social Icons</div>
+        <div class="footer-socials col">
+          <p class="socialIcons float-right">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </p>
+        </div>
       </div>
 
     </div>
